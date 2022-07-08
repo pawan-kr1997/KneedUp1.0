401844517ba65b50945e294b6b0c779604f6709b
Pawan Kumar <65812849+pawan-kr1997@users.noreply.github.com>
2022-07-08 23:22:11 +0530

    refactored index.js

--- server/index.js
+++ server/index.js
@@ -5,17 +5,6 @@ const cors = require('cors');
 const userRoutes = require('./Routes/User');
 const postRoutes = require('./Routes/Post');
 
-// const national = require('./Scheduling/National');
-// const international = require('./Scheduling/International');
-// const business = require('./Scheduling/Business');
-// const sports = require('./Scheduling/Sports');
-// const niti = require('./Scheduling/Nitiblogs');
-// const idsa = require('./Scheduling/Idsa');
-// const pib = require('./Scheduling/Pib');
-// const prsblog = require('./Scheduling/PRSblog');
-// const prsarticle = require('./Scheduling/PRSarticle');
-// const poispeech = require('./Scheduling/PoiSpeeches');
-// const poipress = require('./Scheduling/PoiPress');
 const scheduler = require('./Scheduling/UpdateData');
 const notificationEmail = require('./Scheduling/NotificationEmail');
 const deleteNews = require('./Scheduling/DeleteNews');
@@ -28,20 +17,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// setInterval(scheduler.nationalScheduler, 3600000);
-// setInterval(scheduler.internationalScheduler, 7200000);
-// setInterval(scheduler.businessScheduler, 7200000);
-// setInterval(scheduler.sportsScheduler, 3600000);
-// setInterval(scheduler.nitiScheduler, 86400000);
-// setInterval(scheduler.idsaScheduler, 86400000);
-// setInterval(scheduler.pibScheduler, 3600000);
-// setInterval(scheduler.prsblogScheduler, 86400000);
-// setInterval(scheduler.prsarticleScheduler, 86400000);
-// setInterval(scheduler.poispeechScheduler, 86400000);
-// setInterval(scheduler.poipressScheduler, 86400000);
+ setInterval(scheduler.nationalScheduler, 3600000);
+ setInterval(scheduler.internationalScheduler, 7200000);
+ setInterval(scheduler.businessScheduler, 7200000);
+ setInterval(scheduler.sportsScheduler, 3600000);
+ setInterval(scheduler.nitiScheduler, 86400000);
+ setInterval(scheduler.idsaScheduler, 86400000);
+ setInterval(scheduler.pibScheduler, 3600000);
+ setInterval(scheduler.prsblogScheduler, 86400000);
+ setInterval(scheduler.prsarticleScheduler, 86400000);
+ setInterval(scheduler.poispeechScheduler, 86400000);
+ setInterval(scheduler.poipressScheduler, 86400000);
 
-// setInterval(notificationEmail, 86400000);
-// setInterval(deleteNews, 86400000);
+ setInterval(notificationEmail, 86400000);
+ setInterval(deleteNews, 86400000);
 
 const PORT = process.env.PORT || 8080;
 
